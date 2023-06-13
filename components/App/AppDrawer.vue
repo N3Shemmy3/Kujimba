764bf412f672a9c06f5a28d3f30f77bfb71ee76b
N3Shemmy3 <n3shemmy3@gmail.com>
2023-06-14 01:22:03 +0200

    self explainatory

--- components/App/AppDrawer.vue
+++ components/App/AppDrawer.vue
@@ -1,8 +1,8 @@
 <template>
-	<div ref="overlay">
+	<div ref="overlay" class="absolute md:relative h-screen w-screen md:w-fit">
 		<div
 			ref="drawer"
-			class="h-full w-[250px] shadow-md bg-colorSurfaceLight text-colorOnSurfaceLight dark:bg-colorSurfaceDark dark:text-colorOnSurfaceDark"
+			class="h- w-[250px] shadow-md bg-colorSurfaceLight text-colorOnSurfaceLight dark:bg-colorSurfaceDark dark:text-colorOnSurfaceDark"
 		>
 			<AppToolbar v-if="props.title" :title="props.title" />
 			<slot />
--- components/App/AppRoot.vue
+++ components/App/AppRoot.vue
@@ -2,7 +2,7 @@
 	<div
 		class="flex min-h-screen w-full bg-colorBackgroundLight text-colorOnBackgroundLight dark:bg-colorBackgroundDark dark:text-colorOnBackgroundDark"
 	>
-		<div class="flex mx-auto h-full w-full md:max-w-screen-lg">
+		<div class="flex mx-auto h-full w-full md:max-w-screen-2xl">
 			<slot />
 		</div>
 	</div>
